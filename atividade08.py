04268188a5193d57f795373989519f8b67e8334f
raqbrrs <raquelbrrs07@gmail.com>
2024-09-09 09:01:06 -0300

    reais em dólares

---
+++ atividade08.py
@@ -0,0 +1,8 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+v_reais = float(input("digite o valor em reais"))
+t_cambio = 5.60
+
+conversao = v_reais/t_cambio
+
+print(f'o valor do real em dolares é {conversao}')
+
